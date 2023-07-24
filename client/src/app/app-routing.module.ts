40d44f3abbbfee44fecb1bce7cbd2a6100038add
TsholofeloMakhoana <tsholofelomakhoana@gmail.com>
2023-07-24 16:33:15 +0200

    changes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 import { RegisterComponent } from './components/register/register.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 
 const routes: Routes = [
 
   {path:'register', component: RegisterComponent},
+  {path:'landing', component: LandingPageComponent},
   {path:'',redirectTo: 'home', pathMatch: 'full'} 
 ];
 
