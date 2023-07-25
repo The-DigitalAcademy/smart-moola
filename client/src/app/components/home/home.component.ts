97b57d6fbed49f6e33b2da4c7a0b675e3949cb13
OwethuSotomela <owethusotomela@gmail.com>
2023-07-25 09:33:19 +0200

    Navigating to homepage

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  userTypes = ['debt-free-user', 'indebted'];
+  selectedUserType: string = '';
 
 }
