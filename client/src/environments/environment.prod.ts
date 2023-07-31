c7322f0788d41306aeebd61a19a206ab53c21922
OwethuSotomela <owethusotomela@gmail.com>
2023-07-31 09:02:08 +0200

    Hosted

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
 };
 
-// export let usersAPI: string = 'https://smart-9qg3.onrender.com/api/users'
-export let usersAPI: string = 'http://localhost:4545/api/users'
+export let usersAPI: string = 'https://smart-9qg3.onrender.com/api/users'
+// export let usersAPI: string = 'http://localhost:4545/api/users'
