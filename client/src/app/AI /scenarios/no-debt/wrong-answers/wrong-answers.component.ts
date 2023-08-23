7c64b066a668e24be6b9f85976eaedd8fdb4d918
OwethuSotomela <owethusotomela@gmail.com>
2023-08-23 09:39:13 +0200

    Modified changes, UI and functionality

--- client/src/app/AI /scenarios/no-debt/wrong-answers/wrong-answers.component.ts
+++ client/src/app/AI /scenarios/no-debt/wrong-answers/wrong-answers.component.ts
@@ -5,21 +5,17 @@ import { LoaderService } from 'src/app/services/Loader';
 @Component({
   selector: 'app-wrong-answers',
   templateUrl: './wrong-answers.component.html',
-  styleUrls: ['./wrong-answers.component.css']
+  styleUrls: ['./wrong-answers.component.scss']
 })
 export class WrongAnswersComponent implements OnInit {
 
-  constructor(private router : Router, public loaderService: LoaderService) { }
+  constructor(private router: Router, public loaderService: LoaderService) { }
 
   ngOnInit() {
-
-    
     setTimeout(() => {
       this.loaderService.startLoader()
       this.router.navigate(['/response'])
       this.loaderService.stopLoader();
     }, 3000)
-
   }
-
 }
