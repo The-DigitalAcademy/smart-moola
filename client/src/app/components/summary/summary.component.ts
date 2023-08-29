a5107ed9b5ddf07e6f5fffe238b7fa4891124755
Mbaliqwema94 <129956817+Mbaliqwema94@users.noreply.github.com>
2023-08-29 16:07:29 +0200

    styling

--- client/src/app/components/summary/summary.component.ts
+++ client/src/app/components/summary/summary.component.ts
@@ -4,19 +4,19 @@ import { UsersService } from 'src/app/services/users.service';
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.scss']
+  styleUrls: ['./summary.component.scss'],
 })
 export class SummaryComponent implements OnInit {
+  progressPercentage: any;
 
-  constructor(
-    private usersServices: UsersService
-  ) { }
+  constructor
+  (private usersServices: UsersService) {}
 
   ngOnInit(): void {
+    
   }
 
-  goBack(){
-    this.usersServices.previousPage
+  goBack() {
+    this.usersServices.previousPage();
   }
-
 }
