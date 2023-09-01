a80005b56617d605bf28f8e08010866863a6e372
OwethuSotomela <owethusotomela@gmail.com>
2023-09-01 11:46:25 +0200

    Fixed bugs and errors

--- client/src/app/services/users.service.ts
+++ client/src/app/services/users.service.ts
@@ -79,6 +79,11 @@ export class UsersService {
   }
 
   previousPage() {
-    this.location.back();
+    this.location.back()
+    if (this.location.path() == '/response') {
+      this.location.back()
+    } else {
+      this.location.back();
+    }
   }
 }
