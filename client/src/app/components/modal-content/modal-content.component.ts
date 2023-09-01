9cbbda03178ffc2d788449161ccb3ea472225a75
OwethuSotomela <owethusotomela@gmail.com>
2023-09-01 16:30:48 +0200

    Fixed bugs and errors

--- client/src/app/components/modal-content/modal-content.component.ts
+++ client/src/app/components/modal-content/modal-content.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-modal-content',
   templateUrl: './modal-content.component.html',
-  styleUrls: ['./modal-content.component.css']
+  styleUrls: ['./modal-content.component.scss']
 })
 export class ModalContentComponent {
 
