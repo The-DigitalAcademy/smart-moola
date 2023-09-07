dbc4ece949ca5982d58601c7954bbce663651c3c
Owethu Sotomela <owethusotomela@gmail.com>
2023-09-07 20:33:40 +0200

    Able to get the correct and incorrect responses

--- client/src/app/AI /scenarios/indebtedscenes/indebtedscene.component.ts
+++ client/src/app/AI /scenarios/indebtedscenes/indebtedscene.component.ts
@@ -11,7 +11,6 @@ import { LoaderService } from 'src/app/services/Loader';
   templateUrl: './indebtedscene.component.html',
   styleUrls: ['./indebtedscene.component.scss']
 })
-
 export class IndebtedScenesComponent implements OnInit {
 
   currentSelectionValue = '';
@@ -22,11 +21,12 @@ export class IndebtedScenesComponent implements OnInit {
 
   //FROM NO DEBT USER
   question = '';
-  prompt = ''
-  explanation = ''
+  prompt = '';
+  explanation = '';
   isLoading: boolean = true;
 
-  constructor(private counterService: CounterService,
+  constructor(
+    private counterService: CounterService,
     private router: Router,
     private questionsService: QuestionService,
     private session: SessionsService,
@@ -49,24 +49,14 @@ export class IndebtedScenesComponent implements OnInit {
     "Debt Counselling",
     "Debt Consolidation",
     "Debt Review"
-  ]
+  ];
 
-  //AI Method
   getMeaning() {
     this.questionsService.sendQuestionAndGetExplanation(this.prompt).subscribe(data => {
       console.log(data); // Check the entire response object
       console.log(data.explanation); // Check the explanation property
       this.explanation = data.explanation;
       localStorage.setItem("explanation", this.explanation);
-    });
-  }
-
-  //Is connection to the ai
-  getQuestions() {
-    this.questionsService.getAllQuestions().subscribe((data) => {
-      for (const item of data) {
-        console.log(item.id)
-      }
     });
   }
 
@@ -77,52 +67,36 @@ export class IndebtedScenesComponent implements OnInit {
     }
   }
 
-  onSelectionChange(event: Event) {
-    const selectedValue = (event.target as HTMLInputElement).value;
+  onSelectionChange(selectedValue: string) {
+    this.selectedValue = selectedValue;
+  }
 
-    switch (selectedValue) {
-      case 'selection1':
-        this.selection1();
+  submitForm() {
+    const selectedAnswer = this.selectedValue;
+    const correctAnswer = this.correct[this.statementIndex];
+    this.getMeaning();
 
-        this.prompt = 'What is Debt Counselling?'
-        this.session.saveActiveQuestion('selection2');
-
-        break;
-      case 'selection2':
-        this.selection2();
-
-        this.prompt = 'What is Debt Consolidation?'
-        this.session.saveActiveQuestion('selection3');
-
-        break;
-      case 'selection3':
-        this.selection3();
-
-        this.prompt = 'What is Debt Review?'
-        this.session.saveActiveQuestion('selection3');
-        // this.router.navigate(['/summary2']);
-        break;
-      default:
-
-        this.selection1();
-
-        this.prompt = 'What is Debt Counselling?'
-        this.session.saveActiveQuestion('selection2');
-        break;
+    if (selectedAnswer === correctAnswer) {
+      this.isResponseCorrect = true;
+    } else {
+      this.isResponseCorrect = false;
     }
 
-  }
+    this.statementIndex++;
+    this.loaderService.startLoader();
 
-  selection1() {
-    this.counterService.selection1();
-  }
+    setTimeout(() => {
+      this.router.navigate(['/modal']);
+      this.isLoading = false;
+      this.loaderService.stopLoader();
+    }, 3000);
 
-  selection2() {
-    this.counterService.selection2();
-  }
-
-  selection3() {
-    this.counterService.selection3();
+    if (this.statementIndex < this.statement.length) {
+      this.selectedValue = '';
+      this.router.navigate(['/indebtedscene']);
+    } else {
+      this.router.navigate(['/summary2']);
+    }
   }
 
   getCurrentStatement(): string {
@@ -133,54 +107,16 @@ export class IndebtedScenesComponent implements OnInit {
     return this.questions[this.statementIndex];
   }
 
-  submitForm() {
-    const selectedAnswer = this.selectedValue; // Use selectedValue to check the selected radio button
-    const correctAnswer = this.correct[this.statementIndex];
-    this.getMeaning();
-
-    if (selectedAnswer === correctAnswer) {
-      this.isResponseCorrect = true; // Set response to correct
-    } else {
-      this.isResponseCorrect = false; // Set response to incorrect
-    }
-
-    this.statementIndex++;
-
-    // Start the loader
-    this.loaderService.startLoader();
-
-    // After a delay (for example, 3000 milliseconds), navigate to the modal
-    setTimeout(() => {
-      this.router.navigate(['/modal']);
-
-      // Stop the loader and hide it
-      this.isLoading = false;
-      this.loaderService.stopLoader();
-    }, 3000);
-
-    if (this.statementIndex < this.statement.length) {
-      // If there are more statements/questions, navigate to the next loop
-      this.selectedValue = ''; // Clear the selected radio button
-      this.router.navigate(['/indebtedscene']);
-    } else {
-      // If all statements/questions are done, navigate to "summary2"
-      this.router.navigate(['/summary2']);
-    }
-  }
 
   ngOnInit() {
-
-    this.progressPercentage = 25; // Initialize progress to 0%
-    console.log("Percentage", this.progressPercentage)
+    this.progressPercentage = 25;
     this.updateTextScene();
     this.counterService.currentSelection.subscribe((selectionValue: any) => {
       this.currentSelectionValue = selectionValue;
     });
-
   }
 
   updateTextScene() {
     this.progressPercentage = ((this.statementIndex + 1) / this.statement.length) * 75;
   }
-
 }
